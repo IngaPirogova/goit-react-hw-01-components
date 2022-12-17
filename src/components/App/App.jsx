aa5a518e6d690119a5261f228b81be7dd16ceb94
IngaPirogova <ingeborga.pirogova@gmail.com>
2022-12-17 23:46:44 +0300

    Summary

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,3 +1,24 @@
+import user from '.components/Profile/user.json';
+import { Profile } from "components/Profile/Profile";
+
+
+
 export const App = () => {
-    return <div>App</div>;
+    return (
+    <div>
+        <Profile  
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}    
+        />          
+    </div>
+    );
 };
+
+
+
+
+
+
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,32 +1,48 @@
-const Profile = ({ username, tag, location, avatar, stats }) => {
-    return <div class="profile">
-  <div class="description">
+import PropTypes from 'prop-types';
+import css from './Profile.module.css';
+
+
+export const Profile = ({username, tag, location, avatar, 
+    stats}) => {    
+    return (
+    <div className={css.profile}>
+        <div className={css.description}>
     <img
       src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
       alt="User avatar"
-      class="avatar"
+      className={css.avatar}
     />
-    <p class="name">Petra Marica</p>
-    <p class="tag">@pmarica</p>
-    <p class="location">Salvador, Brasil</p>
-  </div>
-
-  <ul class="stats">
-    <li>
-      <span class="label">Followers</span>
-      <span class="quantity">1000</span>
+    <p className={css.name}>Petra Marica</p>
+    <p className={css.tag}>@pmarica</p>
+    <p className={css.location}>Salvador, Brasil</p>
+  </div>  
+  
+  <ul className={css.stats}>
+    <li className={css.stats__item}>
+      <span className={css.label}>Followers</span>
+      <span className={css.quantity}>1000</span>
     </li>
-    <li>
-      <span class="label">Views</span>
-      <span class="quantity">2000</span>
+    <li className={css.stats__item}>
+      <span className={css.label}>Views</span>
+      <span className={css.quantity}>2000</span>
     </li>
-    <li>
-      <span class="label">Likes</span>
-      <span class="quantity">3000</span>
+    <li className={css.stats__item}>
+      <span className={css.label}>Likes</span>
+      <span className={css.quantity}>3000</span>
     </li>
   </ul>
-</div>
+  
+    </div>
+    
+    );
+};
+     
+Profile.propTypes = {
+    username: PropTypes.string.isRequired, 
+    tag: PropTypes.string.isRequired, 
+    location: PropTypes.string.isRequired, 
+    avatar: PropTypes.string.isRequired, 
+    stats: PropTypes.number.isRequired
+
 }
-
-
 
