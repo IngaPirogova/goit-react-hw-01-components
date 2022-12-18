ab6cce41b966bb8bd4415fcf7c12a8c87195a3ae
IngaPirogova <ingeborga.pirogova@gmail.com>
2022-12-19 00:21:26 +0300

    Summary

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -2,22 +2,25 @@ import PropTypes from 'prop-types';
 import css from './Statistics.module.css';
 
   export const Statistics  = ({ title , stats }) => {
-   
-    return (
+return (
 <section className={css.statistics}> 
-       
+
   {title && <h2 className={css.title}>{title}</h2>}
 
     <ul className={css.stat__list}>
-        {stats.map(({ id, label, percentage }) => (
-            
-          <li className={css.item} key={id}>
+        {stats.map(({ id, label, percentage}) => (
+          
+          <li className={css.item} key={id} style={{backgroundColor: getRandomHexColor()}} >           
+           
             <span className={css.label}>{label}</span>
-            <span className={css.percentage}>{percentage}</span>
+            <span className={css.percentage}>{percentage}%</span>            
           </li>
+          
         ))}    
     </ul>
 </section>
+
+
 )};
 
 Statistics.propTypes = {
@@ -32,4 +35,10 @@ Statistics.propTypes = {
    };
      
 
-
+   function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, 0)}`;
+  
+  }
+  
