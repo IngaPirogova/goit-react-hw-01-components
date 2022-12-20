737c4b7fde5eb1f46c7a3b9d82db0756c0f90ab7
IngaPirogova <ingeborga.pirogova@gmail.com>
2022-12-20 14:13:54 +0300

    summ

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -4,22 +4,19 @@ import css from './FriendListItem.module.css';
 export const FriendListItem = ({ avatar, name, isOnline }) => {
 
     return (
-        <li className={css.item}>
-           <span className={`${css.status} ${css[isOnline]}`}>{isOnline}</span>
-           <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
+    <li className={css.item}>
+           <span className={`${css.status} ${css[isOnline]}`}>{isOnline}</span>           
+           <img className={css.avatar} src={avatar} alt="User avatar" width="48" />           
            <p className={css.name}>{name}</p>
-        </li>
+    </li>
     )
+    
 }
 
 FriendListItem.propTypes = {
-    friends: PropTypes.arrayOf(
-        PropTypes.exact({  
-            
+        
             avatar: PropTypes.string.isRequired,   
             name: PropTypes.string.isRequired,
-            id: PropTypes.number.isRequired,
             isOnline: PropTypes.bool.isRequired,
-    }),
-    ),    
-};
+    
+}
