9eee2b68149d010c9b5987fede0619bcdedf03cc
IngaPirogova <ingeborga.pirogova@gmail.com>
2022-12-20 14:04:03 +0300

    upd

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,25 +1,25 @@
-// import PropTypes from 'prop-types';
-// import css from './FriendListItem.module.css';
+import PropTypes from 'prop-types';
+import css from './FriendListItem.module.css';
 
-// export const FriendListItem = ({ avatar, name, isOnline }) => {
+export const FriendListItem = ({ avatar, name, isOnline }) => {
 
-//     return (
-//         <li className={css.item}>
-//            <span className={`${css.status} ${css[isOnline]}`}>{isOnline}</span>
-//            <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
-//            <p className={css.name}>{name}</p>
-//         </li>
-//     )
-// }
+    return (
+        <li className={css.item}>
+           <span className={`${css.status} ${css[isOnline]}`}>{isOnline}</span>
+           <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
+           <p className={css.name}>{name}</p>
+        </li>
+    )
+}
 
-// FriendListItem.propTypes = {
-//     friends: PropTypes.arrayOf(
-//         PropTypes.exact({  
+FriendListItem.propTypes = {
+    friends: PropTypes.arrayOf(
+        PropTypes.exact({  
             
-//             avatar: PropTypes.string.isRequired,   
-//             name: PropTypes.string.isRequired,
-//             id: PropTypes.number.isRequired,
-//             isOnline: PropTypes.bool.isRequired,
-//     }),
-//     ),    
-// };
+            avatar: PropTypes.string.isRequired,   
+            name: PropTypes.string.isRequired,
+            id: PropTypes.number.isRequired,
+            isOnline: PropTypes.bool.isRequired,
+    }),
+    ),    
+};
