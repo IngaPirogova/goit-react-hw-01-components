d19840ae3e0a87bc948b5a6952f6b85fa192e6b8
IngaPirogova <ingeborga.pirogova@gmail.com>
2022-12-20 14:23:42 +0300

    Summary

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -14,7 +14,7 @@ export const FriendListItem = ({ avatar, name, isOnline }) => {
 }
 
 FriendListItem.propTypes = {
-        
+
             avatar: PropTypes.string.isRequired,   
             name: PropTypes.string.isRequired,
             isOnline: PropTypes.bool.isRequired,
